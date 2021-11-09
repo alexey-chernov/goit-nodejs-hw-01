62dadd9dff0ef0330131ef0ef74172f17431f205
alexey-chernov <32240525+alexey-chernov@users.noreply.github.com>
2021-11-09 21:14:46 +0200

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,46 @@
+const fs = require("fs/promises");
+const path = require("path");
+const crypto = require("crypto");
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+const readContacts = async () => {
+    try {
+        const result = await fs.readFile(contactsPath, "utf8");
+        const contacts = JSON.parse(result);
+        return contacts;
+    } catch (error) {
+        return console.error(error.message);
+    }
+};
+
+function listContacts() {
+    return readContacts();
+}
+
+async function getContactById(contactId) {
+    const contacts = await readContacts();
+    const result = contacts.filter((contact) => contact.id === Number(contactId));
+    return result;
+}
+
+async function removeContact(contactId) {
+    const contacts = await readContacts();
+    const result = contacts.filter((contact) => contact.id !== Number(contactId));
+    await fs.writeFile(contactsPath, JSON.stringify(result, null, 2))
+    return result;
+
+}
+
+async function addContact(name, email, phone) {
+    const contacts = await readContacts();
+    const newContact = { id: crypto.randomUUID(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2)
+    );
+    return newContact;
+}
+
+module.exports = {
+    listContacts, getContactById, removeContact, addContact
+}
---
+++ index.js
@@ -0,0 +1,64 @@
+const { Command } = require('commander');
+const chalk = require('chalk');
+const {
+        listContacts, getContactById, addContact, removeContact
+    } = require('./contacts');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      listContacts()
+        .then((contacts) => console.table(contacts))
+          .catch(console.error);
+      break;
+
+    case 'get':
+        getContactById(id)
+        .then(contact => {
+            if (contact) {
+               console.log(chalk.green('Contact found!'));
+               console.table(contact);
+            } else {
+                console.log(chalk.yellow('Contact not found!'));
+            }
+            
+          })
+      .catch(console.error);
+      break;
+
+    case 'add':
+        addContact(name, email, phone)
+          .then(contacts => {
+              console.log(chalk.blue('Add new contact!'));
+              console.table(contacts);
+            })
+        .catch(console.error);
+      break;
+
+    case 'remove':
+      removeContact(id)
+        .then(contact => {
+           console.log(chalk.green('Contact delete!'));
+             console.table(contact);
+                
+        })
+       .catch(console.error);
+      break;
+
+    default:
+      console.warn(chalk.red('Unknown action type!'));
+  }
+}
+
+invokeAction(argv);
